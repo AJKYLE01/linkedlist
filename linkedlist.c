23238704d5b44dec93a6144f4bc060cd297b060d
Ryan Laiphrakpam <ryanlaiphrakpam123@gmail.com>
2023-12-27 03:30:32 +0530

    insertion added

--- linkedlist.c
+++ linkedlist.c
@@ -4,7 +4,40 @@ typedef struct node{
 	int data;
 	struct node* temp;
 } node;
+node* insert(node *head){
+	node* temp;
+	temp=(node*)malloc(sizeof(node));
+	int a;
+	printf("Enter the value to be inserted : ");
+	scanf("%d",&a);
+	temp->data=a;
+	temp->next=NULL;
+	if(head==NULL){
+		head=temp;
+		return head;
+	}
+	node* p1=head;
+	while(p1->next!=NULL){
+		p1=p1->next;
+	}
+	p1->next=temp;
+	return head;
+}
 int main(){
 	node* head;
 	head=(node*)malloc(sizeof(node));
+	head=NULL;
+	char ch;
+	printf("\t\tLinked List");
+	printf("\n\tFor insertion \t---- i");
+	printf("\n\nEnter your choice : ");
+	scanf("%c",&ch);
+	switch(ch){
+		case 'i':
+			head=insert(head);
+			break;
+		default:
+			printf("\n\tInvalid Entry.");
+			break;
+	}
 }
